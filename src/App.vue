88c26900b728bff1783c4246adc033bc340f8abd
Admin <9684173+zwh_3_0@user.noreply.gitee.com>
2022-02-14 22:22:36 +0800

    development

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,13 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
 import HeaderVue from "./components/Header/Header.vue";
 import TabVue from "./components/Tab/Tab.vue";
-export default defineComponent({
+export default {
     components:{HeaderVue,TabVue},
-})
+}
 </script>
 
 <style lang="less">
-
+    
 </style>
