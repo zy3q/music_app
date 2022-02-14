254a9b6214b5410bcf9296d7b4240377feea6f08
Admin <9684173+zwh_3_0@user.noreply.gitee.com>
2022-02-14 11:35:22 +0800

    development

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,9 @@
+<template>
+头部
+</template>
+<script>
+    import { defineComponent } from "vue";
+    export default defineComponent({
+        name:"Header",
+    })
+</script>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+//全局引入样式文件
+import './assets/less/index.less'
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount('#app')
